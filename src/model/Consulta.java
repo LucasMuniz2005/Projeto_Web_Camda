8d14fb22a1477f9ee44b98ccc875b33a21cb740d
LucasMuni2005 <lucasmunizmiranda@gmail.com>
2026-05-22 12:38:55 -0300

    Grupo 13 - Correções após revisão do professor

--- src/model/Consulta.java
+++ src/model/Consulta.java
@@ -16,10 +16,10 @@ public class Consulta {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "data_hora_agendamento")
+    @Column(name = "data_hora_agendamento",nullable = false)
     private LocalDateTime dataHoraAgendamento;
 
-    @Column(name = "data_hora_efetiva")
+    @Column(name = "data_hora_efetiva", nullable = false)
     private LocalDateTime dataHoraEfetiva;
 
     @Column(name = "descricao", columnDefinition = "TEXT")
@@ -34,85 +34,210 @@ public class Consulta {
     private Paciente paciente;
 
     @ManyToOne
-    @JoinColumn(name = "usuario_id")
+    @JoinColumn(name = "id_usuario_cancelamento")
     private Usuario canceladaPor;
+    
+    
+    
+    @ManyToOne
+    @JoinColumn(name = "id_usuario_registro")
+    private Usuario registrouPor;
 
+    
+    
     @ManyToOne
     @JoinColumn(name = "configuracao_perfil_profissional_saude_id")
-    private ConfiguracaoPerfilProfissionalSaude foiRegistradaPor;
+    private ConfiguracaoPerfilProfissionalSaude foiAtendidoPor;
+
+   
+    @OneToMany(mappedBy = "consulta", cascade = CascadeType.ALL)
+    private List<ResultadoExame> listaResultadosExame = new ArrayList<>();
+
+    @OneToMany
+    private List<RegistroDoenca> listaRegistrosDoenca = new ArrayList<>();
+
+    @OneToMany
+    private List<Posologia> listaPosologias = new ArrayList<>();
+
+    @OneToMany
+    private List<TipoExame> listaTiposExame = new ArrayList<>();
 
     @ManyToOne
-    @JoinColumn(name = "plano_id")
-    private Plano plano;
-
-    @OneToMany(mappedBy = "consulta", cascade = CascadeType.ALL)
-    private List<ResultadoExame> resultadosExame = new ArrayList<>();
-
-    @OneToMany(mappedBy = "consulta", cascade = CascadeType.ALL)
-    private List<Reserva> reservas = new ArrayList<>();
-
-    @ManyToMany
-    @JoinTable(
-        name = "consulta_registro_doenca",
-        joinColumns = @JoinColumn(name = "consulta_id"),
-        inverseJoinColumns = @JoinColumn(name = "registro_doenca_id")
-    )
-    private List<RegistroDoenca> registrosDoenca = new ArrayList<>();
-
-    @ManyToMany
-    @JoinTable(
-        name = "consulta_posologia",
-        joinColumns = @JoinColumn(name = "consulta_id"),
-        inverseJoinColumns = @JoinColumn(name = "posologia_id")
-    )
-    private List<Posologia> posologias = new ArrayList<>();
+    @JoinColumn(name = "sala_atendimento_id")
+    private SalaAtendimento salaAtendimento;
 
     public Consulta() {}
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    
+    
+    public Long getId() {
+        return id;
+    }
 
-    public LocalDateTime getDataHoraAgendamento() { return dataHoraAgendamento; }
-    public void setDataHoraAgendamento(LocalDateTime dataHoraAgendamento) { this.dataHoraAgendamento = dataHoraAgendamento; }
+    public void setId(Long i) throws ModelException {
+        if(i == null)
+            throw new ModelException("O id não pode ser nulo!");
+        this.id = i;
+    }
 
-    public LocalDateTime getDataHoraEfetiva() { return dataHoraEfetiva; }
-    public void setDataHoraEfetiva(LocalDateTime dataHoraEfetiva) { this.dataHoraEfetiva = dataHoraEfetiva; }
+    public LocalDateTime getDataHoraAgendamento() {
+        return dataHoraAgendamento;
+    }
 
-    public String getDescricao() { return descricao; }
-    public void setDescricao(String descricao) { this.descricao = descricao; }
+    public void setDataHoraAgendamento(LocalDateTime d) throws ModelException {
+        if(d == null)
+            throw new ModelException("A data e hora de agendamento não pode ser nula!");
+        this.dataHoraAgendamento = d;
+    }
 
-    public StatusConsulta getStatus() { return status; }
-    public void setStatus(StatusConsulta status) { this.status = status; }
+    public LocalDateTime getDataHoraEfetiva() {
+        return dataHoraEfetiva;
+    }
 
-    public Paciente getPaciente() { return paciente; }
-    public void setPaciente(Paciente paciente) { this.paciente = paciente; }
+    public void setDataHoraEfetiva(LocalDateTime d) throws ModelException {
+        if(d == null)
+            throw new ModelException("A data e hora efetiva não pode ser nula!");
+        this.dataHoraEfetiva = d;
+    }
 
-    public Usuario getCanceladaPor() { return canceladaPor; }
-    public void setCanceladaPor(Usuario canceladaPor) { this.canceladaPor = canceladaPor; }
+    public String getDescricao() {
+        return descricao;
+    }
 
-    public ConfiguracaoPerfilProfissionalSaude getFoiRegistradaPor() { return foiRegistradaPor; }
-    public void setFoiRegistradaPor(ConfiguracaoPerfilProfissionalSaude foiRegistradaPor) { this.foiRegistradaPor = foiRegistradaPor; }
+    public void setDescricao(String d) throws ModelException {
+        if(d == null || d.isBlank())
+            throw new ModelException("A descrição não pode ser nula ou vazia!");
+        this.descricao = d;
+    }
 
-    public Plano getPlano() { return plano; }
-    public void setPlano(Plano plano) { this.plano = plano; }
+    public StatusConsulta getStatus() {
+        return status;
+    }
 
-    public List<ResultadoExame> getResultadosExame() { return new ArrayList<>(resultadosExame); }
-    public void setResultadosExame(List<ResultadoExame> resultadosExame) { this.resultadosExame = resultadosExame; }
-    public void adicionarResultadoExame(ResultadoExame r) { this.resultadosExame.add(r); }
-    public void removerResultadoExame(ResultadoExame r) { this.resultadosExame.remove(r); }
+    public void setStatus(StatusConsulta s) throws ModelException {
+        if(s == null)
+            throw new ModelException("O status não pode ser nulo!");
+        this.status = s;
+    }
 
-    public List<Reserva> getReservas() { return new ArrayList<>(reservas); }
-    public void setReservas(List<Reserva> reservas) { this.reservas = reservas; }
-    public void adicionarReserva(Reserva r) { this.reservas.add(r); }
-    public void removerReserva(Reserva r) { this.reservas.remove(r); }
+    public Paciente getPaciente() {
+        return paciente;
+    }
 
-    public List<RegistroDoenca> getRegistrosDoenca() { return new ArrayList<>(registrosDoenca); }
-    public void setRegistrosDoenca(List<RegistroDoenca> registrosDoenca) { this.registrosDoenca = registrosDoenca; }
-    public void adicionarRegistroDoenca(RegistroDoenca r) { this.registrosDoenca.add(r); }
-    public void removerRegistroDoenca(RegistroDoenca r) { this.registrosDoenca.remove(r); }
+    public void setPaciente(Paciente p) throws ModelException {
+        if(p == null)
+            throw new ModelException("O paciente não pode ser nulo!");
+        this.paciente = p;
+    }
 
-    public List<Posologia> getPosologias() { return new ArrayList<>(posologias); }
-    public void setPosologias(List<Posologia> posologias) { this.posologias = posologias; }
-    public void adicionarPosologia(Posologia p) { this.posologias.add(p); }
-    public void removerPosologia(Posologia p) { this.posologias.remove(p); }
+    public Usuario getCanceladaPor() {
+        return canceladaPor;
+    }
+
+    public void setCanceladaPor(Usuario u) {
+        this.canceladaPor = u;
+    }
+
+    public Usuario getRegistrouPor() {
+        return registrouPor;
+    }
+
+    public void setRegistrouPor(Usuario u) throws ModelException {
+        if(u == null)
+            throw new ModelException("O usuário que registrou não pode ser nulo!");
+        this.registrouPor = u;
+    }
+
+    public ConfiguracaoPerfilProfissionalSaude getFoiAtendidoPor() {
+        return foiAtendidoPor;
+    }
+
+    public void setFoiAtendidoPor(ConfiguracaoPerfilProfissionalSaude c) throws ModelException {
+        if(c == null)
+            throw new ModelException("O profissional de saúde não pode ser nulo!");
+        this.foiAtendidoPor = c;
+    }
+
+    public SalaAtendimento getSalaAtendimento() {
+        return salaAtendimento;
+    }
+
+    public void setSalaAtendimento(SalaAtendimento s) throws ModelException {
+        if(s == null)
+            throw new ModelException("A sala de atendimento não pode ser nula!");
+        this.salaAtendimento = s;
+    }
+
+    public List<ResultadoExame> getListaResultadosExame() {
+        return new ArrayList<ResultadoExame>(this.listaResultadosExame);
+    }
+
+    public void setListaResultadosExame(List<ResultadoExame> novaLista) throws ModelException {
+        if(novaLista == null)
+            throw new ModelException("A lista de resultados de exame não pode ser nula!");
+        this.listaResultadosExame = novaLista;
+    }
+
+    public boolean adicionarResultadoExame(ResultadoExame r) {
+        return this.listaResultadosExame.add(r);
+    }
+
+    public boolean removerResultadoExame(ResultadoExame r) {
+        return this.listaResultadosExame.remove(r);
+    }
+
+    public List<RegistroDoenca> getListaRegistrosDoenca() {
+        return new ArrayList<RegistroDoenca>(this.listaRegistrosDoenca);
+    }
+
+    public void setListaRegistrosDoenca(List<RegistroDoenca> novaLista) throws ModelException {
+        if(novaLista == null)
+            throw new ModelException("A lista de registros de doença não pode ser nula!");
+        this.listaRegistrosDoenca = novaLista;
+    }
+
+    public boolean adicionarRegistroDoenca(RegistroDoenca r) {
+        return this.listaRegistrosDoenca.add(r);
+    }
+
+    public boolean removerRegistroDoenca(RegistroDoenca r) {
+        return this.listaRegistrosDoenca.remove(r);
+    }
+
+    public List<Posologia> getListaPosologias() {
+        return new ArrayList<Posologia>(this.listaPosologias);
+    }
+
+    public void setListaPosologias(List<Posologia> novaLista) throws ModelException {
+        if(novaLista == null)
+            throw new ModelException("A lista de posologias não pode ser nula!");
+        this.listaPosologias = novaLista;
+    }
+
+    public boolean adicionarPosologia(Posologia p) {
+        return this.listaPosologias.add(p);
+    }
+
+    public boolean removerPosologia(Posologia p) {
+        return this.listaPosologias.remove(p);
+    }
+
+    public List<TipoExame> getListaTiposExame() {
+        return new ArrayList<TipoExame>(this.listaTiposExame);
+    }
+
+    public void setListaTiposExame(List<TipoExame> novaLista) throws ModelException {
+        if(novaLista == null)
+            throw new ModelException("A lista de tipos de exame não pode ser nula!");
+        this.listaTiposExame = novaLista;
+    }
+
+    public boolean adicionarTipoExame(TipoExame t) {
+        return this.listaTiposExame.add(t);
+    }
+
+    public boolean removerTipoExame(TipoExame t) {
+        return this.listaTiposExame.remove(t);
+    }
+    
 }
---
+++ src/model/ModelException.java
@@ -0,0 +1,7 @@
+package model;
+
+public class ModelException extends Exception {
+    public ModelException(String mensagem) {
+        super(mensagem);
+    }
+}
